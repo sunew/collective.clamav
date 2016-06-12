c2f222d927f8b30e547e1e7766df1c579f97b313
andreasma <maand@gmx.de>
2016-06-12 15:48:27 +0200

    Added all fields again.

--- src/collective/clamav/interfaces.py
+++ src/collective/clamav/interfaces.py
@@ -21,14 +21,38 @@ clamdConnectionType = SimpleVocabulary(
 class IAVScannerSettings(Interface):
     """ Schema for the clamav settings
     """
+    clamav_connection = schema.Choice(
+        title=_(u"Connection type to clamd"),
+        description=_(u"Choose whether clamd is accessible through local "
+                      u"UNIX sockets or network."),
+        vocabulary=clamdConnectionType)
 
+    clamav_socket = schema.ASCIILine(
+        title=_(u"Clamd local socket file"),
+        description=_(u"If connected to clamd through local UNIX sockets, "
+                      u"the path to the local socket file."),
+        default='/var/run/clamd',
+        required=True)
 
-clamav_connection = schema.Choice(
-    title=_(u"Connection type to clamd"),
-    description=_(u"Choose whether clamd is "
-                  u"accessible through local "
-                  u"UNIX sockets or network."),
-    vocabulary=clamdConnectionType)
+    clamav_host = schema.ASCIILine(title=_(u"Scanner host"),
+                                   description=_(u"If connected to clamd "
+                                                 u"through the network, "
+                                                 u"the host running clamd."),
+                                   default='localhost',
+                                   required=True)
+
+    clamav_port = schema.Int(title=_(u"Scanner port"),
+                             description=_(u"If connected to clamd "
+                                           u"through the network, the "
+                                           u"port on which clamd listens."),
+                             default=3310,
+                             required=True)
+
+    clamav_timeout = schema.Int(title=_(u"Timeout"),
+                                description=_(u"The timeout in seconds for "
+                                              u"communication with clamd."),
+                                default=120,
+                                required=True)
 
 
 class IAVScanner(Interface):
