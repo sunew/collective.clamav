3d56d44405c6d86859d7b4988448f9618482c796
andreasma <maand@gmx.de>
2016-06-12 20:14:50 +0200

    Adding a blank line.

--- src/collective/clamav/__init__.py
+++ src/collective/clamav/__init__.py
@@ -5,6 +5,7 @@ from zope.i18nmessageid import MessageFactory
 from Products.validation import validation
 from collective.clamav.validator import ClamavValidator
 
+
 _ = MessageFactory('collective.clamav')
 
 
