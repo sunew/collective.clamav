387ca45bf36623171b02ed85445a6648369a64f9
andreasma <maand@gmx.de>
2016-06-12 10:50:17 +0200

    unicode fixes

--- src/collective/clamav/interfaces.py
+++ src/collective/clamav/interfaces.py
@@ -13,8 +13,8 @@ class ICollectiveClamavLayer(IDefaultBrowserLayer):
 
 
 clamdConnectionType = SimpleVocabulary(
-    [SimpleTerm(value=u"Local UNIX Socket", title="socket"),
-     SimpleTerm(value=u"Network", title="net")]
+    [SimpleTerm(value=u"Local UNIX Socket", title=u"socket"),
+     SimpleTerm(value=u"Network", title=u"net")]
 )
 
 
@@ -24,31 +24,31 @@ class IAVScannerSettings(Interface):
     clamav_connection = schema.Choice(
         title=_(u"Connection type to clamd"),
         description=_(u"Choose whether clamd is accessible through local "
-                      "UNIX sockets or network."),
+                      u"UNIX sockets or network."),
         vocabulary=clamdConnectionType)
 
     clamav_socket = schema.ASCIILine(
         title=_(u"Clamd local socket file"),
         description=_(u"If connected to clamd through local UNIX sockets, "
-                      "the path to the local socket file."),
+                      u"the path to the local socket file."),
         default='/var/run/clamd',
         required=True)
 
     clamav_host = schema.ASCIILine(title=_(u"Scanner host"),
                                    description=_(u"If connected to clamd through the network, "
-                                                 "the host running clamd."),
+                                                 u"the host running clamd."),
                                    default='localhost',
                                    required=True)
 
     clamav_port = schema.Int(title=_(u"Scanner port"),
                              description=_(u"If connected to clamd through the network, "
-                                           "the port on which clamd listens."),
+                                           u"the port on which clamd listens."),
                              default=3310,
                              required=True)
 
     clamav_timeout = schema.Int(title=_(u"Timeout"),
                                 description=_(u"The timeout in seconds for communication with "
-                                              "clamd."),
+                                              u"clamd."),
                                 default=120,
                                 required=True)
 
