6c97873b0ec2dcaf986ee5defa470a7f72853078
andreasma <maand@gmx.de>
2016-06-12 20:22:18 +0200

    Another try to fix the import issue.

--- src/collective/clamav/__init__.py
+++ src/collective/clamav/__init__.py
@@ -6,7 +6,7 @@ from Products.validation import validation
 from collective.clamav.validator import ClamavValidator
 
 
-_ = MessageFactory('collective.clamav')
+_=MessageFactory('collective.clamav')
 
 
 validation.register(ClamavValidator('isVirusFree'))
