12ec74ee90c5d24466298b67ddd87ffa1014d65e
andreasma <maand@gmx.de>
2016-06-12 13:19:28 +0200

    pep8-fixes

--- src/collective/clamav/interfaces.py
+++ src/collective/clamav/interfaces.py
@@ -35,20 +35,22 @@ class IAVScannerSettings(Interface):
         required=True)
 
     clamav_host = schema.ASCIILine(title=_(u"Scanner host"),
-                                   description=_(u"If connected to clamd through the network, "
+                                   description=_(u"If connected to clamd "
+                                                 u"through the network, "
                                                  u"the host running clamd."),
                                    default='localhost',
                                    required=True)
 
     clamav_port = schema.Int(title=_(u"Scanner port"),
-                             description=_(u"If connected to clamd through the network, "
-                                           u"the port on which clamd listens."),
+                             description=_(u"If connected to clamd "
+                                           u"through the network, the "
+                                           u"port on which clamd listens."),
                              default=3310,
                              required=True)
 
     clamav_timeout = schema.Int(title=_(u"Timeout"),
-                                description=_(u"The timeout in seconds for communication with "
-                                              u"clamd."),
+                                description=_(u"The timeout in seconds for "
+                                              u"communication with clamd."),
                                 default=120,
                                 required=True)
 
