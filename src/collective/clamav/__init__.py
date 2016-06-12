b10f4ba15ee57a59a182292196e4530a0b59cf03
andreasma <maand@gmx.de>
2016-06-12 22:40:48 +0200

    pep8-fix, removed unnecessary import.

--- src/collective/clamav/__init__.py
+++ src/collective/clamav/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
-
 from zope.i18nmessageid import MessageFactory
-from Products.validation import validation
 
 
-_=MessageFactory('collective.clamav')
+_ = MessageFactory('collective.clamav')
 
 
 
