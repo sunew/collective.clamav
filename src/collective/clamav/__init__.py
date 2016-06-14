0d011b5f14e833a72651c557aff75b569f0b9c06
andreasma <maand@gmx.de>
2016-06-14 19:30:34 +0200

    Added the import and the validation registration again, but in the second place.

--- src/collective/clamav/__init__.py
+++ src/collective/clamav/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 from zope.i18nmessageid import MessageFactory
+from Products.validation import validation
 
 
 _ = MessageFactory('collective.clamav')
+
+
+from collective.clamav.validator import ClamavValidator
+
+validation.register(ClamavValidator('isVirusFree'))
